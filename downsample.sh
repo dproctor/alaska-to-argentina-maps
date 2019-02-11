ee50153c67ac2f45a5852eaa46b3e5c25c651428
Devon Proctor <devon@proctors.us>
2019-02-10 22:44:34 -0500

    fix bug in downsample script that was outputing header information as part of sampled lines

--- downsample.sh
+++ downsample.sh
@@ -1,1 +1,1 @@
-DS_FACTOR=$2; FILENAME=$1; echo -e "$(head -n 100 $FILENAME | grep \<)" '\n' "$(awk -v dsf="$DS_FACTOR" 'int(NR/1)%dsf==0' $FILENAME)" '\n' "$(tail -n 100 $FILENAME | grep \<)"
+DS_FACTOR=$2; FILENAME=$1; echo -e "$(head -n 100 $FILENAME | grep \<)" '\n' "$(grep -v \< $FILENAME | awk -v dsf="$DS_FACTOR" 'int(NR/1)%dsf==0')" '\n' "$(tail -n 100 $FILENAME | grep \<)"
